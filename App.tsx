ba2b134acbbc30ef3b1989ebd5d9e2bd70e4e78a
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-08-10 11:18:12 -0300

    fix(prisma): fixing init

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { extendedClient, initializeDb } from './utils/prisma';
 
-export default function App() {
+function AppInitialized() {
+  const users = extendedClient.user.useFindMany();
+
+  console.log(users)
+
+  useEffect(() => {
+    extendedClient.user.create({
+      data: {
+        name: 'teste'
+      }
+    })
+    .then((res) => console.log('res: ', res))
+    .catch(console.log)
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    initializeDb().then(() => setDbInitialized(true)).catch(console.log)
+  }, [])
+
+  if (!dbInitialized) return <ActivityIndicator />
+
+  return <AppInitialized />
 }
 
 const styles = StyleSheet.create({
